bed55ab6582bae369284c59ce56510d967d44709
ESG <eribertosg.dev@gmail.com>
2023-02-26 19:15:31 -0600

    Added the functionality to close the menu when clicking outside.

--- js/main.js
+++ js/main.js
@@ -2,7 +2,16 @@ const openMenu = document.getElementById('open-menu')
 const lateralMenu = document.getElementById('lateral-menu')
 const menuBackground = document.getElementById('menu-background')
 
-openMenu.addEventListener('click', () => {
+const openAndClose = () => {
     lateralMenu.classList.toggle('show-menu')
     menuBackground.classList.toggle('show-menu')
+}
+openMenu.addEventListener('click', () => {
+    openAndClose()
+})
+
+window.addEventListener('click', e => {
+    if (e.target == menuBackground) {
+        openAndClose()
+    }
 })
